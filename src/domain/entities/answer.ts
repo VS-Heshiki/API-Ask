ca6befd6ba6b06054e1539f476be9a3598784f2d
Victor Heshiki <victor_heshiki@hotmail.com>
2023-06-27 19:17:31 -0300

    feat: implement getters and setters

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -2,8 +2,32 @@ import { Entity, UniqueEntityId } from '@/core/entities'
 import { Optional } from '@/core/types'
 
 export class Answer extends Entity<AnswerInput>{
+    get authorId () {
+        return this.params.authorId
+    }
+
+    get questionId () {
+        return this.params.questionId
+    }
+
     get content () {
         return this.params.content
+    }
+
+    get excerpt () {
+        return this.content
+            .substring(0, 120)
+            .trimEnd()
+            .concat('...')
+    }
+
+    private refresh () {
+        this.params.updatedAt = new Date()
+    }
+
+    set content (content: string) {
+        this.content = content
+        this.refresh()
     }
 
     static create (params: Optional<AnswerInput, 'createdAt'>, id?: UniqueEntityId): Answer {
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -3,9 +3,57 @@ import { Optional } from '@/core/types'
 import { Slug } from '@/domain/entities/value-objects'
 
 export class Question extends Entity<QuestionInput> {
-    static create (params: Optional<QuestionInput, 'createdAt'>, id?: UniqueEntityId): Question {
+    get authorId () {
+        return this.params.authorId
+    }
+
+    get bestAnswerId () {
+        return this.params.bestAnswerId
+    }
+
+    get title () {
+        return this.params.title
+    }
+
+    get content () {
+        return this.params.content
+    }
+
+    get slug () {
+        return this.params.slug
+    }
+
+    get excerpt () {
+        return this.content
+            .substring(0, 120)
+            .trimEnd()
+            .concat('...')
+    }
+
+    private refresh () {
+        this.params.updatedAt = new Date()
+    }
+
+    set bestAnswerId (bestAnswerId: UniqueEntityId | undefined) {
+        this.params.bestAnswerId = bestAnswerId
+        this.refresh()
+    }
+
+    set content (content: string) {
+        this.params.content = content
+        this.refresh()
+    }
+
+    set title (title: string) {
+        this.params.title = title
+        this.params.slug = Slug.createFromText(title)
+        this.refresh()
+    }
+
+    static create (params: Optional<QuestionInput, 'createdAt' | 'slug'>, id?: UniqueEntityId): Question {
         const question = new Question({
             ...params,
+            slug: params.slug ?? Slug.createFromText(params.title),
             createdAt: new Date()
         }, id)
 
@@ -15,9 +63,9 @@ export class Question extends Entity<QuestionInput> {
 
 export type QuestionInput = {
     authorId: UniqueEntityId
-    bestAnswerId: UniqueEntityId
-    content: string
+    bestAnswerId?: UniqueEntityId
     title: string
+    content: string
     slug: Slug
     createdAt: Date
     updatedAt?: Date
