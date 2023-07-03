05002a405cade28d659d9e7915e89b65e07e6d64
Victor Heshiki <victor_heshiki@hotmail.com>
2023-07-03 20:11:38 -0300

    test: ensure DeleteCommentOnQuestion Service delete comment on a question

---
+++ tests/domain/forum/application/services/delete-question-comment.spec.ts
@@ -0,0 +1,30 @@
+import { DeleteCommentOnQuestionService } from '@/domain/forum/application/services'
+import { UniqueEntityId } from '@/core/entities'
+import { QuestionCommentRepositoryStub, createQuestionComment } from '@/tests/mock'
+import { QuestionComment } from '@/domain/forum/enterprise/entities'
+
+describe('DeleteQuestionComment Service', () => {
+    let sut: DeleteCommentOnQuestionService
+    let questionCommentRepositoryStub: QuestionCommentRepositoryStub
+    let newQuestionComment: QuestionComment
+
+    beforeEach(async () => {
+        questionCommentRepositoryStub = new QuestionCommentRepositoryStub()
+        sut = new DeleteCommentOnQuestionService(questionCommentRepositoryStub)
+
+        newQuestionComment = createQuestionComment({ authorId: new UniqueEntityId('author-1') }, new UniqueEntityId('comment-1'))
+        await questionCommentRepositoryStub.create(newQuestionComment)
+    })
+
+    it('should call Service with correct values', async () => {
+        const deleteSpy = vitest.spyOn(questionCommentRepositoryStub, 'delete')
+
+        await sut.execute({
+            authorId: 'author-1',
+            commentId: 'comment-1'
+        })
+
+        expect(questionCommentRepositoryStub.items).toHaveLength(0)
+        expect(deleteSpy).toHaveBeenCalledWith(newQuestionComment)
+    })
+})
